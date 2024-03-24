8e8a37e73bb2f6f9367e01dfabe018aff9c7777e
kubik20083 <kubik20083@mail.ru>
2024-03-24 21:48:48 +0300

    initial commit

---
+++ task1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num1, num2, num3, num4, num5;
+    scanf("%d%d%d%d%d", &num1, &num2, &num3, &num4, &num5);
+    printf("greatest number: \n");
+    return 0;
+}
