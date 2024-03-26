b80b3c22645565f07e003ad94239ad19f9add516
kubik20083 <kubik20083@mail.ru>
2024-03-26 20:55:36 +0300

    task1

--- task1.c
+++ task1.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 
+// Задача 1. Наибольшее из пяти чисел
+#include<stdio.h>
+
 int main(int argc, char const *argv[])
 {
     int num1, num2, num3, num4, num5;
     scanf("%d%d%d%d%d", &num1, &num2, &num3, &num4, &num5);
-    printf("greatest number: \n");
+    int max = num1 > num2 ? num1: num2;
+    max = max > num3 ? max : num3;
+    max = max > num4 ? max : num4;
+    max = max > num5 ? max : num5;
+    printf("greatest number : %d\n", max);
+
     return 0;
 }
